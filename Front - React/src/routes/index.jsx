f4aa9052c45ceef4ff04510e84cf723caad6187a
gabrielres10 <gabrielrestrepo7@gmail.com>
2023-10-31 14:12:32 -0500

    feat: add navbar

--- Front - React/src/routes/index.jsx
+++ Front - React/src/routes/index.jsx
@@ -1,19 +1,38 @@
-import {Route, Routes, BrowserRouter  } from  'react-router-dom'
-import  Home from  "../pages/Home"
-import  Login from  "../pages/Login"
-import BooksList from '../pages/BooksList'
-import AuthorsList from '../pages/AuthorsList'
-import AuthorBooksList from '../pages/AuthorBooksList'
-const  Router = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/books" element={<BooksList />}/>
-            <Route path="/authors" element={<AuthorsList />}/>
-            <Route path="/authors/:authorId/books" element={<AuthorBooksList />}/>
-        </Routes>
-    </BrowserRouter>
-)
+import { Route, Routes, BrowserRouter, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import BooksList from '../pages/BooksList';
+import AuthorsList from '../pages/AuthorsList';
+import AuthorBooksList from '../pages/AuthorBooksList';
+import '../styles/styles.css';
 
-export default Router
+const Router = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+        <li>
+          <Link to="/authors">Authors</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/books" element={<BooksList />} />
+      <Route path="/authors" element={<AuthorsList />} />
+      <Route path="/authors/:authorId/books" element={<AuthorBooksList />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
